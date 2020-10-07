e621daec2ab1dee8732cfe45f393ea033299d41e
sgcarlos1 <carlos_fontes1@hotmail.com>
2020-10-07 11:13:19 -0300

    alterando conexao

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', '24102410');
-define('DB', 'login');
+define('HOST', 'mysql669.umbler.com:41890');
+define('USUARIO', 'sgcarlos');
+define('SENHA', 'Carlos2410');
+define('DB', 'sigame');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar');
